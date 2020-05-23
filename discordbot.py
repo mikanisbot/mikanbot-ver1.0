cb028f92e520057f62c4f19158e49be1f3307781
mikanisbot <65799911+mikanisbot@users.noreply.github.com>
2020-05-23 23:49:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/とりあえず動いてます')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
